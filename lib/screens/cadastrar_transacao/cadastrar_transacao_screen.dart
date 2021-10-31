401b70bc12d98d8fb1f83eb9c899876cc4d429a4
Jairo Filho <jairofilho79@gmail.com>
2021-10-31 17:24:10 -0300

    adding date picker

--- lib/screens/cadastrar_transacao/cadastrar_transacao_screen.dart
+++ lib/screens/cadastrar_transacao/cadastrar_transacao_screen.dart
@@ -1,3 +1,4 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:my_contas/models/conta.dart';
 import 'package:my_contas/services/conta_service.dart';
@@ -21,6 +22,7 @@ class _CadastrarTransacaoScreenState extends State<CadastrarTransacaoScreen> {
   ContaService cs = ContaService();
   late Future<List> _loadContas;
   late List<Conta> _contas;
+  DateTime selectedDate = DateTime.now();
 
   @override
   void initState() {
@@ -63,11 +65,19 @@ class _CadastrarTransacaoScreenState extends State<CadastrarTransacaoScreen> {
                         decoration:
                             const InputDecoration(labelText: 'R\$ 00,00'),
                       ),
-                      TextFormField(
-                        controller: _dataController,
-                        keyboardType: TextInputType.datetime,
-                        decoration:
-                            const InputDecoration(labelText: '00/00/0000'),
+                      GestureDetector(
+                        onTap: () => _selectDate(context),
+                        child: AbsorbPointer(
+                          child: TextFormField(
+                            controller: _dataController,
+                            decoration: InputDecoration(
+                              labelText: formatDate(
+                                selectedDate,
+                                [dd, '/', mm, '/', yyyy],
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
                       DropdownButtonFormField(
                         value: _contaSelecionada,
@@ -110,6 +120,21 @@ class _CadastrarTransacaoScreenState extends State<CadastrarTransacaoScreen> {
         },
       ),
     );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2018, 01),
+      lastDate: DateTime(2030, 01),
+    );
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 
   Future<List> _getContas() async {
